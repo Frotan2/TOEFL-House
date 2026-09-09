68cdaefeef7dc69e61b2a103cbaf3f7cf7803abe
Frotan2 <purehands2026@gmail.com>
2026-09-09 18:19:38 +0430

    style(integrations): satisfy current Pint class attribute spacing

--- app/Modules/Integrations/Jobs/IntegrationRetrySweepJob.php
+++ app/Modules/Integrations/Jobs/IntegrationRetrySweepJob.php
@@ -21,7 +21,9 @@ final class IntegrationRetrySweepJob implements JobHandler
     private const DEFAULT_BATCH = 100;
     private const MAX_BATCH = 500;
 
-    public function __construct(private readonly DeliveryProcessor $processor) {}
+    public function __construct(
+        private readonly DeliveryProcessor $processor,
+    ) {}
 
     /**
      * @param  array<string, mixed>  $context
