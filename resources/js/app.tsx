39bfa099850fe4f31040a49190c7fab14d7a478b
Frotan2 <purehands2026@gmail.com>
2026-09-09 15:38:36 +0430

    feat(ux): route front office workspace

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,12 +4,14 @@ import './experience-home.css';
 import './academic-setup.css';
 import './student-journey.css';
 import './teacher-day.css';
+import './front-office.css';
 import './command-palette.css';
 import { AcademicApp } from './academic';
 import { AcademicSetupApp } from './academic-setup';
 import { TeacherApp } from './teacher';
 import { TeacherDayApp } from './teacher-day';
 import { CrmApp } from './crm';
+import { FrontOfficeApp } from './front-office';
 import { ManagementApp } from './management';
 import { StudentJourneyApp } from './student-journey';
 import { StudentsApp } from './students';
@@ -27,24 +29,27 @@ if (root) {
   const academicSetupRequested = view === 'academic' && query.get('view') === 'setup';
   const studentJourneyRequested = view === 'students' && query.get('view') === 'journey' && root.getAttribute('data-student-id') !== null && root.getAttribute('data-student-id') !== '';
   const teacherDayRequested = view === 'teachers' && query.get('view') === 'day';
+  const frontOfficeRequested = view === 'crm' && query.get('view') === 'front-office';
   const content = academicSetupRequested
     ? <AcademicSetupApp {...common} />
     : studentJourneyRequested
       ? <StudentJourneyApp {...common} studentId={root.getAttribute('data-student-id') ?? ''} />
       : teacherDayRequested
         ? <TeacherDayApp {...common} />
-        : view === 'academic'
-          ? <AcademicApp {...common} />
-          : view === 'teachers'
-            ? <TeacherApp {...common} />
-            : view === 'crm'
-              ? <CrmApp {...common} />
-              : view === 'management'
-                ? <ManagementApp {...common} />
-                : view === 'identity'
-                  ? <IdentityApp {...common} />
-                  : view === 'students'
-                    ? <StudentsApp {...common} studentsView={root.getAttribute('data-students-view') ?? 'directory'} studentId={root.getAttribute('data-student-id') ?? ''} />
-                    : <WorkspaceApp {...common} />;
+        : frontOfficeRequested
+          ? <FrontOfficeApp {...common} />
+          : view === 'academic'
+            ? <AcademicApp {...common} />
+            : view === 'teachers'
+              ? <TeacherApp {...common} />
+              : view === 'crm'
+                ? <CrmApp {...common} />
+                : view === 'management'
+                  ? <ManagementApp {...common} />
+                  : view === 'identity'
+                    ? <IdentityApp {...common} />
+                    : view === 'students'
+                      ? <StudentsApp {...common} studentsView={root.getAttribute('data-students-view') ?? 'directory'} studentId={root.getAttribute('data-student-id') ?? ''} />
+                      : <WorkspaceApp {...common} />;
   createRoot(root).render(content);
 }
