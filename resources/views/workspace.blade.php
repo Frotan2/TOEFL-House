b7ac45e40faaaf7e13f77290e2cdea2f00147e94
Frotan2 <purehands2026@gmail.com>
2026-09-09 16:06:42 +0430

    feat(ux): load placement workspace visual contract

--- resources/views/workspace.blade.php
+++ resources/views/workspace.blade.php
@@ -8,6 +8,7 @@
     @vite('resources/js/product-theme.css')
     <link rel="stylesheet" href="{{ asset('css/toefl-house-ultimate.css') }}">
     <link rel="stylesheet" href="{{ asset('css/toefl-house-route-state.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/toefl-house-placement.css') }}">
     @if (($view ?? 'workspace') === 'reporting')
         @vite('resources/js/reporting.tsx')
     @else
